09a7ee552b5b6c522074c68d8944a3a524765ad9
Elin Ölund Forsling <33157767+ElinOlundForsling@users.noreply.github.com>
2021-02-01 13:55:38 +0100

    setting up backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -25,15 +25,31 @@ const ProductScreen = ({ match }) => {
         </section>
         <section className='product-desc'>
           <h2>{product.name}</h2>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+          <hr />
           <p>{product.description}</p>
+          <h5>Price: {product.price}</h5>
+          <hr />
+          {(() => {
+            if (amount === product.countInStock) {
+              return <p className='product-stock'>Maximum amount reached</p>;
+            } else if (product.countInStock === 0) {
+              return <p className='product-stock'>Sold out</p>;
+            } else {
+              return <p className='product-stock'>This product is in stock</p>;
+            }
+          })()}
           <button
-            className='fas fa-plus amount-button'
+            disabled={product.countInStock === 0}
+            className='fas fa-minus amount-button'
             onClick={() =>
-              setAmount(amount =>
-                amount < product.countInStock ? amount + 1 : amount,
-              )
+              setAmount(amount => (amount > 1 ? amount - 1 : amount))
             }></button>
           <input
+            disabled={product.countInStock === 0}
             type='number'
             min='1'
             max={product.countInStock}
@@ -41,15 +57,17 @@ const ProductScreen = ({ match }) => {
             onChange={handleChange}
           />
           <button
-            className='fas fa-minus amount-button'
+            className='fas fa-plus amount-button'
+            disabled={product.countInStock === 0}
             onClick={() =>
-              setAmount(amount => (amount > 1 ? amount - 1 : amount))
+              setAmount(amount =>
+                amount < product.countInStock ? amount + 1 : amount,
+              )
             }></button>
-          <button className='cart-button'>Add to cart</button>
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
+          <button className='cart-button' disabled={product.countInStock === 0}>
+            Add to cart{' '}
+            {Math.round((product.price * amount + Number.EPSILON) * 100) / 100}
+          </button>
         </section>
       </div>
       <Link to='/'>Go back</Link>
